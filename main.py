d349c06401d8e024bf3243cdca6162bc8b365bbd
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-13 22:49:32 -0800

    added the foreground color and images

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ import tkinter
 #TODO: the checkmark will remove the word/definition from the stack
 #TODO: give a background to the mainscreen
 #TODO: use the JSON format to store the words/definition from the users data
+import background as background
+
+BACKGROUND_COLOR = "#B1DDC6"
 
 WINDOW_NAME = "The Flash"
 # generate a window using the tkinter class
@@ -17,9 +20,19 @@ window = tkinter.Tk()
 # change the name of the window
 window.title(WINDOW_NAME)
 # set the size of the padding between canvas and the windows
-window.config(padx=50, pady=50)
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 # window.geometry("800x526")
-canvas = tkinter.Canvas(width=800, height=526)
+canvas = tkinter.Canvas(width=850, height=530)
+flash_image = tkinter.PhotoImage(file="./img/card_front.png")
+canvas.create_image(420, 270, image=flash_image)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(column=0, row=0, columnspan=2)
+right_image = tkinter.PhotoImage(file="./img/right.png")
+right_button = tkinter.Button(image=right_image, highlightthickness=0)
+right_button.grid(column=0, row=1)
+wrong_image = tkinter.PhotoImage(file="./img/wrong.png")
+wrong_button = tkinter.Button(image=wrong_image, highlightthickness=0)
+wrong_button.grid(column=1, row=1)
 # center the window upon opening
 window.eval('tk::PlaceWindow . center')
 # loop the main window to stay open
