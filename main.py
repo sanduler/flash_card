84063a45d9273240b213d7884fb1093a4f420842
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-13 22:13:22 -0800

    added the canvas to the mainscreen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Ruben Sanduleac
 
-
+import tkinter
 # --------------------------------- UI -----------------
 #TODO: user tkinter to build the mainscreen
 #TODO: implement the canvas --> where the word and the definition will be placed on the
@@ -11,3 +11,16 @@
 #TODO: give a background to the mainscreen
 #TODO: use the JSON format to store the words/definition from the users data
 
+WINDOW_NAME = "The Flash"
+# generate a window using the tkinter class
+window = tkinter.Tk()
+# change the name of the window
+window.title(WINDOW_NAME)
+# set the size of the padding between canvas and the windows
+window.config(padx=50, pady=50)
+# window.geometry("800x526")
+canvas = tkinter.Canvas(width=800, height=526)
+# center the window upon opening
+window.eval('tk::PlaceWindow . center')
+# loop the main window to stay open
+window.mainloop()
